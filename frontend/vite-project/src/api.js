6ac89e24db0d269870b8f0b757076f9bb9472ce0
prateeksteaparty <shindepratik624@gmail.com>
2026-02-15 17:26:38 +0530

    trying 2

--- frontend/vite-project/src/api.js
+++ frontend/vite-project/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://vital-backend-q28o.onrender.com/api",
+  baseURL: "https://vital-wellness.onrender.com/api",
   withCredentials: true
 });
 
